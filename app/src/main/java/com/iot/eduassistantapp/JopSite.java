e3a618182bf68ddd8e6c5ed7aef2c8e6478d1a51
ted4858 <ted4858@naver.com>
2022-07-13 15:20:18 +0900

    Initial commit

---
+++ app/src/main/java/com/iot/eduassistantapp/JopSite.java
@@ -0,0 +1,14 @@
+package com.iot.eduassistantapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JopSite extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jop_site);
+    }
+}
